d3ca29e3945fb86c2565cbbba368eea63d26bd4c
Arnold-In-Tech <g16a7782@gmail.com>
2023-12-18 23:47:46 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+
 function addingEventListener() {
+    const input = document.getElementById('button'); // OR querySelector("input")
+    
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+    
+    input.addEventListener('click', clickAlert);
+
 }
+
+addingEventListener();
